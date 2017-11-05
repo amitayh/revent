cabe18274413da866c0b193936c92f862dde9591
Amitay Horwitz <amitayh@gmail.com>
2017-11-05 18:50:52 +0200

    Moved aggregate snapshot reducer

--- src/main/scala/org/revent/AggregateSnapshot.scala
+++ src/main/scala/org/revent/AggregateSnapshot.scala
@@ -2,6 +2,9 @@ package org.revent
 
 import java.time.Instant
 
+import org.revent.AggregateSnapshot.InitialVersion
+import org.revent.Reducer.AggregateReducer
+
 import scala.language.higherKinds
 
 case class AggregateSnapshot[Aggregate](aggregate: Aggregate,
@@ -15,4 +18,19 @@ case class AggregateSnapshot[Aggregate](aggregate: Aggregate,
 
 object AggregateSnapshot {
   val InitialVersion = 0
+}
+
+class SnapshotReducer[P <: Protocol](aggregateReducer: AggregateReducer[P])
+  extends Reducer[AggregateSnapshot[P#Aggregate], Event[P#EventStream]] {
+
+  override val empty: AggregateSnapshot[P#Aggregate] =
+    AggregateSnapshot(aggregateReducer.empty, InitialVersion)
+
+  override def handle(snapshot: AggregateSnapshot[P#Aggregate],
+                      event: Event[P#EventStream]): AggregateSnapshot[P#Aggregate] =
+    AggregateSnapshot(
+      aggregate = aggregateReducer.handle(snapshot.aggregate, event.payload),
+      version = event.version,
+      timestamp = Some(event.timestamp))
+
 }
--- src/main/scala/org/revent/Reducer.scala
+++ src/main/scala/org/revent/Reducer.scala
@@ -1,8 +1,5 @@
 package org.revent
 
-import org.revent.AggregateSnapshot.InitialVersion
-import org.revent.Reducer.AggregateReducer
-
 trait Reducer[State, Event] {
   def empty: State
   def handle(state: State, event: Event): State
@@ -10,19 +7,4 @@ trait Reducer[State, Event] {
 
 object Reducer {
   type AggregateReducer[P <: Protocol] = Reducer[P#Aggregate, P#EventPayload]
-}
-
-class SnapshotReducer[P <: Protocol](aggregateReducer: AggregateReducer[P])
-  extends Reducer[AggregateSnapshot[P#Aggregate], Event[P#EventStream]] {
-
-  override val empty: AggregateSnapshot[P#Aggregate] =
-    AggregateSnapshot(aggregateReducer.empty, InitialVersion)
-
-  override def handle(snapshot: AggregateSnapshot[P#Aggregate],
-                      event: Event[P#EventStream]): AggregateSnapshot[P#Aggregate] =
-    AggregateSnapshot(
-      aggregate = aggregateReducer.handle(snapshot.aggregate, event.payload),
-      version = event.version,
-      timestamp = Some(event.timestamp))
-
 }
